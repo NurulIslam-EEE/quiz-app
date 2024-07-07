d837373096521a441d04e6e68342edef652a31f5
NurulIslam-EEE <nurul.islam02310@gmail.com>
2024-07-07 13:04:10 +0600

    setting up question and result component

--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import ResultTable from "@/components/ResultTable";
+import Link from "next/link";
+import "../../styles/result.css";
 
 function Result() {
-  return <div>Result</div>;
+  const flag = true;
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application</h1>
+
+      <div className="result flex-center">
+        <div className="flex">
+          <span>Username</span>
+          <span className="bold">0</span>
+        </div>
+        <div className="flex">
+          <span>Total Quiz Points : </span>
+          <span className="bold">0</span>
+        </div>
+        <div className="flex">
+          <span>Total Questions : </span>
+          <span className="bold">0</span>
+        </div>
+        <div className="flex">
+          <span>Total Attempts : </span>
+          <span className="bold">0</span>
+        </div>
+        <div className="flex">
+          <span>Total Earn Points : </span>
+          <span className="bold">0</span>
+        </div>
+        <div className="flex">
+          <span>Quiz Result</span>
+          <span
+            style={{ color: `${flag ? "#2aff95" : "#ff2a66"}` }}
+            className="bold"
+          >
+            {flag ? "Passed" : "Failed"}
+          </span>
+        </div>
+      </div>
+
+      <div className="start">
+        <Link className="btn" href={"/"}>
+          Restart
+        </Link>
+      </div>
+
+      <div className="container">
+        {/* result table */}
+        <ResultTable></ResultTable>
+      </div>
+    </div>
+  );
 }
 
 export default Result;
